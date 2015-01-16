e8f0a80093c2b2ebb26359d51b5e4b29c3a8b7a5
mramirez <jxc876@gmail.com>
2015-01-16 16:55:39 -0600

    Lockdown deletion & save methods

--- src/main/groovy/org/mramirez/repo/MemberRepo.groovy
+++ src/main/groovy/org/mramirez/repo/MemberRepo.groovy
@@ -1,19 +1,24 @@
 package org.mramirez.repo
 
-import org.mramirez.model.ChatRoom
 import org.mramirez.model.Member
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.RepositoryDefinition
 import org.springframework.data.repository.query.Param
 import org.springframework.security.access.prepost.PreAuthorize
 
+@RepositoryDefinition(domainClass=Member.class, idClass=Long.class)
+interface MemberRepo {
 
-interface MemberRepo extends CrudRepository<Member, Long> {
-
-	@PreAuthorize("hasRole('ROLE_USER')")
-	Iterable<ChatRoom> findAll()
-	
-	@PreAuthorize("hasRole('ROLE_USER')")
+	Member findOne(Long id)
+	List<Member> findAll()
 	List<Member> findByName(@Param("name") String name)
-	
 	List<Member> findByAlias(@Param("alias") String alias)
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	Member save(Member entity);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	void delete(Long id);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	void delete(Member member);
 }

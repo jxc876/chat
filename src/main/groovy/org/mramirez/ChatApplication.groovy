8d177bb72a6a468b55b7b9ed2f9c4158f4d9ac36
mramirez <jxc876@gmail.com>
2015-01-16 16:57:28 -0600

    Move Security config to own file

--- src/main/groovy/org/mramirez/ChatApplication.groovy
+++ src/main/groovy/org/mramirez/ChatApplication.groovy
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct
 import org.h2.tools.Server
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration
@@ -20,11 +21,10 @@ class ChatApplication {
 	static void main(String[] args) {
 		SpringApplication.run ChatApplication, args
 	}
-	
+
 	@PostConstruct
 	void h2WebServer(){
 		println '**************** starting H2 Web Server ****************'
 		Server.createWebServer("-web","-webAllowOthers", "-webPort", "8082").start()
 	}
-	
 }
---
+++ src/main/groovy/org/mramirez/Security.groovy
@@ -0,0 +1,26 @@
+package org.mramirez
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+class Security extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+				.authorizeRequests().anyRequest().authenticated()
+				.and()
+				.formLogin().and()
+				.httpBasic().and().csrf().disable();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("user").password("password123").roles("USER").and()
+				.withUser("admin").password("password123").roles("ADMIN")
+	}
+}

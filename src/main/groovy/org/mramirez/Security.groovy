56235fc87076701f96748f58db24e088cab7a810
mramirez <jxc876@gmail.com>
2015-01-26 14:28:22 -0600

    disable sessions

--- src/main/groovy/org/mramirez/Security.groovy
+++ src/main/groovy/org/mramirez/Security.groovy
@@ -4,15 +4,16 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 class Security extends WebSecurityConfigurerAdapter {
-
+                                 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http
-				.authorizeRequests().anyRequest().authenticated()
-				.and().httpBasic().and().csrf().disable();
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic()
+				.and().csrf().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 	}
 
 	@Override

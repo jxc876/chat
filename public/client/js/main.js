a085907d89b83cfb6b658e06277f5452d3504d78
jxc876 <jxc876@gmail.com>
2015-02-07 18:33:00 -0600

    created Backbone member view

--- public/client/js/main.js
+++ public/client/js/main.js
@@ -1,21 +1,30 @@
 // main.js
 $(function(){
 	
-
-// Note: use JSON.stringify() if you need to debug
-
 	var Member = Backbone.Model.extend({
 		urlRoot : 'http://localhost:8080/members',
 		defaults: { "name" : "", "alias" : ""}
+	});
+
+	var MemberView = Backbone.View.extend({
+	    el: $('#members'),
+		template: _.template($('#memberTemplate').html()),
+	    render: function(){
+	        var newHtml = this.template(this.model.toJSON());
+	        this.$el.append(newHtml);
+	        return this;
+	    }
 	});
 
 	var memberOne = new Member({id:1});
 	
 	memberOne.fetch({
 		success : function(member){
-			alert(JSON.stringify(member));
+			//alert(JSON.stringify(member));
+			var app = new MemberView({ model: member });
+			app.render();
 		}
 	});
-
+	
 
 });
